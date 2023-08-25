f0892de9e1f8cc10add931720fda0136667af9e1
khang25102008 <tkngs08@gmail.com>
2023-08-25 19:57:02 +0700

    fix User Interface(UI) of navbar, login page, signup page

--- store/form.py
+++ store/form.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Your Username',
-        'class': 'w-full py-4 px-6 rounded-xl'
+        'class': 'w-100 p-3 rounded-xl'
     }))
     
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Your Password',
-        'class': 'w-full py-4 px-6 rounded-xl'
+        'class': 'w-100 p-3 rounded-xl'
     }))
 
 class SignupForm(UserCreationForm):
@@ -20,20 +20,20 @@ class SignupForm(UserCreationForm):
     
     username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Your Username',
-        'class': 'w-full py-4 px-6 rounded-xl'
+        'class': 'w-100 p-3 rounded-xl'
     }))
     
     email = forms.CharField(widget=forms.EmailInput(attrs={
         'placeholder': 'Your Email',
-        'class': 'w-full py-4 px-6 rounded-xl'
+        'class': 'w-100 p-3 rounded-xl'
     }))
     
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Your Password',
-        'class': 'w-full py-4 px-6 rounded-xl'
+        'class': 'w-100 p-3 rounded-xl'
     }))
     
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Repeat Password',
-        'class': 'w-full py-4 px-6 rounded-xl'
+        'class': 'w-100 p-3 rounded-xl'
     }))
